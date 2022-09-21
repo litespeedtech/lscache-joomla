c3bff528e6a2f52da0c9718c18263d2082f23d51
WuhuaChen <34863158+WuhuaChen@users.noreply.github.com>
2022-09-21 14:37:39 -0400

    [Update] Articles and article categories now will recache seo url

--- Joomla4/lscache_plugin/components/com_content.php
+++ Joomla4/lscache_plugin/components/com_content.php
@@ -6,6 +6,7 @@
  *  @copyright  Copyright (c) 2017-2018 LiteSpeed Technologies, Inc. (https://www.litespeedtech.com)
  *  @license    https://opensource.org/licenses/GPL-3.0
  */
+use Joomla\Component\Content\Site\Helper\RouteHelper;
 
 class LSCacheComponentContent extends LSCacheComponentBase
 {
@@ -15,8 +16,28 @@ class LSCacheComponentContent extends LSCacheComponentBase
     {
         $comUrls =  array();
         $db = JFactory::getDbo();
+
+//  get category urls
         $query = $db->getQuery(true)
-                ->select($db->quoteName('id'))
+                ->select($db->quoteName(array('id','language')))
+                ->from('#__categories');
+        $query->where($db->quoteName('published') . ' =1');
+        $query->where($db->quoteName('extension') . ' ="com_content"');
+
+        try {
+            $db->setQuery($query);
+            $categories = $db->loadObjectList();
+            foreach($categories as $category){
+                $link =  RouteHelper::getCategoryRoute($category->id, $category->language);
+                $comUrls[] = $link;
+            }
+        } catch (RuntimeException $ex) {
+            return array();
+        }
+
+//  get article urls;
+        $query = $db->getQuery(true)
+                ->select($db->quoteName(array('id','language','catid','alias')))
                 ->from('#__content');
         $query->where($db->quoteName('state') . ' =1');
 
@@ -25,9 +46,13 @@ class LSCacheComponentContent extends LSCacheComponentBase
             $articles = $db->loadObjectList();
             foreach($articles as $article){
                 if(empty($article->id)){
-                    $article->id = 0;
+                    continue;
                 }
-                $comUrls[] = 'index.php?option=com_content&view=article&id=' . $article->id;
+                $slug = $article->alias ? ($article->id . ':' . $article->alias) : $article->id;
+
+                $link = RouteHelper::getArticleRoute($slug, $article->catid, $article->language);
+                
+                $comUrls[] = $link;
             }
         } catch (RuntimeException $ex) {
             return array();
