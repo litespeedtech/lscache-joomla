e723bcb4602624deaac9c9033a55caf7d5f4319f
WuhuaChen <WCHEN@LITESPEEDTECH.COM>
2024-08-01 09:36:48 -0400

    update latest packages

--- Joomla3/package/buildPackage.sh
+++ Joomla3/package/buildPackage.sh
@@ -5,13 +5,12 @@ cd ../com_lscache
 zip -r ../package/build/com_lscache.zip ./*
 cd ../lscache_plugin
 zip -r ../package/build/lscache_plugin.zip ./*
-cd ../mod_lscache
-zip -r ../package/build/mod_lscache.zip ./*
+cd ../mod_lscache_purge
+zip -r ../package/build/mod_lscache_purge.zip ./*
 cp -r ../esiTemplate ../package/build/
-
 cd ../package/build
 zip -r ../lscache-latest.zip ./*
 cd ..
 rm -f ./build/*.zip
 rm -rf ./build/esiTemplate
-echo Latest package has been built to lscache-latest.zip
+echo Latest package has been built to lscache-latest.
